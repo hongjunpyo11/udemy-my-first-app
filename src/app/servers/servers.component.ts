129ba491ff08f33c2b53704d8ac316d92b1b4749
hongjunpyo11 <jpyo98@naver.com>
2023-11-27 21:26:50 +0900

    이벤트 바인딩으로 데이터 전달하기와 사용하기

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNerServer = false;
   serverCreationStatus: string = 'No server was created!';
+  serverName: string = '';
 
   constructor() {
     setTimeout(() => {
@@ -25,4 +26,7 @@ export class ServersComponent {
     this.serverCreationStatus = 'Server was created!';
   }
 
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
